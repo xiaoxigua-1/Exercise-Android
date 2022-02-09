f6239879de6c917d0e1090aabce0085da675eeb0
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-02-09 16:26:23 +0800

    hide login bar

--- app/src/main/java/com/example/exerciseandroid/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/exerciseandroid/ui/login/LoginActivity.kt
@@ -24,7 +24,7 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        supportActionBar?.hide()
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
